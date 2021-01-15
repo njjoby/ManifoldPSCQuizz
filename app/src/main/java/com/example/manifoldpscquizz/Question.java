c872a4a26465fb22b347c96de25f340935c073ab
njjoby <njjoby@gmail.cok>
2021-01-15 13:58:36 +0530

    from college

---
+++ app/src/main/java/com/example/manifoldpscquizz/Question.java
@@ -0,0 +1,17 @@
+package com.example.manifoldpscquizz;
+
+import java.util.ArrayList;
+
+public class Question {
+
+
+    public String questionWithOptions;
+    public String answer;
+    public ArrayList<String> Belongs;
+    public String descriptionIfAny;
+    public String examName ;
+    public String postName;
+    public String year;
+
+
+}
